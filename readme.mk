604c885bcd367828b23bf564666db25dfb2507b0
Raul Barriga <raulbarriga658@gmail.com>
2025-10-06 18:43:25 +0200

    Primer pas de la web

---
+++ readme.mk
@@ -0,0 +1,5 @@
+Els alumnes crearan una pàgina web personal senzilla. Aquesta ha de contenir:
+El seu nom o identificació.
+Una breu descripció o frase personal.
+Una imatge o avatar opcional.
+Alguns estils bàsics aplicats amb CSS.
